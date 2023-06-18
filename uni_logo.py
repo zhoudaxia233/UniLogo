64ca8c63799181551c89f350436ca1a3d774977e
zhoudaxia <yootaoo+git@gmail.com>
2023-06-18 17:17:10 +0200

    fixed formatting issues and bugs when path is absent

--- uni_logo.py
+++ uni_logo.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 
 class UniRanking:
     def __init__(self):
-        self.base_url = 'https://www.shanghairanking.com'
+        self.base_url = "https://www.shanghairanking.com"
         self.api_url = f"{self.base_url}/api/pub/v1/arwu/rank?version=2022"
         self.rankings = None
         self.uni_rank = {}  # {uni_name: ranking}
@@ -15,7 +15,7 @@ class UniRanking:
         if not self.rankings:
             response = requests.get(self.api_url, timeout=5)
             data = response.json()
-            self.rankings = data['data']['rankings']
+            self.rankings = data["data"]["rankings"]
         return self.rankings
 
     def download_uni_logo(self, num_uni=1000, path=None) -> None:
@@ -24,28 +24,33 @@ class UniRanking:
 
         self._check_img_folder(path)
 
-        savepath = Path(path)
+        save_path = path or Path.cwd() / Path("images")
 
         for rank in tqdm(self.rankings[:num_uni]):
-            uni_name, ranking_str, short_path = rank['univNameEn'], rank['ranking'], rank['univLogo']
+            uni_name, ranking_str, short_path = (
+                rank["univNameEn"],
+                rank["ranking"],
+                rank["univLogo"],
+            )
             self.uni_rank[uni_name] = ranking_str
 
             rsp = requests.get(f"{self.base_url}/_uni/{short_path}", timeout=5)
 
-            with open(savepath/ f"{uni_name}{Path(short_path).suffix}", 'wb') as f:
+            with open(
+                Path(save_path) / f"{uni_name}{Path(short_path).suffix}", "wb"
+            ) as f:
                 f.write(rsp.content)
         print("Done.")
 
     def _check_img_folder(self, path=None) -> None:
-        if path is None:
-          folder_path = Path.cwd() / Path("images")
-        else:
-          folder_path=Path(path)
+        path = path or Path.cwd() / Path("images")
+        folder_path = Path(path)
+
         if not folder_path.exists():
             folder_path.mkdir()
-            print("Folder 'images' created.")
+            print(f"Folder '{folder_path.name}' created.")
         else:
-            print("Folder 'images' already exists.")
+            print(f"Folder '{folder_path.name}' already exists.")
 
 
 if __name__ == "__main__":
